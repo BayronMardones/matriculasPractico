c0c2465c13065ca2cfda3fe75e6a84498fadb05b
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2024-03-19 01:35:54 -0300

    inicio frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import { setupDB } from "./config/db.config.js";
 import cors from "cors";
 import studentRoutes from "./api/routes/studentRoutes.js";
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 
 app.use(json());
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import BasicButtons from "./components/prueba"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <BasicButtons />
+    </div>
+  )
+}
+
+export default App
