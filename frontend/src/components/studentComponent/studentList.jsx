b869a5c993465d06200675e1d0dc2150f61f1e42
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2024-03-21 00:24:40 -0300

    sweetalert eliminar student

--- frontend/src/components/studentComponent/studentList.jsx
+++ frontend/src/components/studentComponent/studentList.jsx
@@ -6,7 +6,9 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const StudentList = ({ students }) => {
+import PropTypes from 'prop-types';
+
+const StudentList = ({ students, onDelete }) => {
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -15,6 +17,9 @@ const StudentList = ({ students }) => {
                         <TableCell>NOMBRE</TableCell>
                         <TableCell align="right">APELLIDO</TableCell>
                         <TableCell align="right">APODO</TableCell>
+                        <TableCell align="right">FECHA NACIMIENTO</TableCell>
+                        <TableCell align="right">RUT</TableCell>
+                        <TableCell align="right">EMAIL</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -27,13 +32,36 @@ const StudentList = ({ students }) => {
                                 {student.nombres}
                             </TableCell>
                             <TableCell align="right">{student.apellidos}</TableCell>
-                            <TableCell align="right">{student.apodo}</TableCell> {/* Agrega la celda para el apodo */}
-                        </TableRow>
+                            <TableCell align="right">{student.apodo}</TableCell>
+                            <TableCell align="right">{student.fechaNacimiento}</TableCell>
+                            <TableCell align="right">{student.rut}</TableCell>
+                            <TableCell align="right">{student.email}</TableCell>
+                            <TableCell>
+                            <button onClick={() => onDelete(student._id)}>Eliminar</button>
+                            </TableCell>
+                        </TableRow>     
                     ))}
                 </TableBody>
             </Table>
         </TableContainer>
     );
 }
+
+//validacion de props
+
+StudentList.propTypes = {
+    students: PropTypes.arrayOf(
+        PropTypes.shape({
+            _id: PropTypes.string.isRequired,
+            nombres: PropTypes.string.isRequired,
+            apellidos: PropTypes.string,
+            apodo: PropTypes.string,
+            fechaNacimiento: PropTypes.string,
+            rut: PropTypes.string,
+            email: PropTypes.string,
+        })
+    ).isRequired,
+    onDelete: PropTypes.func.isRequired,
+};
 
 export default StudentList;
--- frontend/src/pages/studentPage/studentPage.jsx
+++ frontend/src/pages/studentPage/studentPage.jsx
@@ -1,5 +1,6 @@
-import StudentList from "./studentList";
+import StudentList from "../../components/studentComponent/studentList.jsx";
 import { useEffect, useState } from "react";
+import Swal from 'sweetalert2'
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const StudentPage = () => {
@@ -23,11 +24,55 @@ const StudentPage = () => {
         fetchStudents();
     } , []);
 
+    const deleteStudent = async (id) => {
+        try {
+        const result = await Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        });
+
+        if (result.isConfirmed) {
+            const response = await fetch(`${apiUrl}/students/deleteStudent/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                await response.json();
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+                const newStudents = students.filter(student => student._id !== id);
+                setStudents(newStudents);
+            } else {
+                throw new Error("Failed to delete student");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        Swal.fire({
+            title: "Error!",
+            text: "Failed to delete student.",
+            icon: "error"
+        });
+    }
+}
+
+
     
     return (
         <>
             <h1>Pagina Estudiantes</h1>
-            <StudentList students={students} />
+            <StudentList students={students} onDelete={deleteStudent}/>
         </>
 
     );
